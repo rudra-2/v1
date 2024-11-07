aef9fdb933af2c24d74de8a2ebb9f556f6bec556
rudra-2 <rmp20026@gmail.com>
2024-11-07 19:37:30 +0530

    Basic Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import ProfileAnima from './components/ProfileAnima';
+import Description from './components/Description';
+import JoinHNM from './components/JoinHNM';
+import Steps from './components/Steps';
+import TGScreen from './components/TGScreen';
+import Timeline from './components/Timeline';
 import './App.css';
-
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <ProfileAnima />
+      <Description />
+      <Timeline />
+      <Steps />
+      <TGScreen />
+      <JoinHNM />
     </div>
   );
-}
+};
 
 export default App;
